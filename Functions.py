c3afd1bc1406008a72a533bb9d134cdf00e3f2e7
Ramcharan Abhilash <ramcharanabhilash@gmail.com>
2026-01-04 22:26:52 +0530

    Readded the file code

---
+++ Functions.py
@@ -0,0 +1,17 @@
+def sum(name, **data):
+    print(name)
+
+    for i,j in data.items():
+
+        print(i,j)
+
+sum('Charan',City='Chirala', mob=885473)
+
+def div(a,b):
+    print(a/b)
+div(4,2)
+
+a = 10
+b = 30
+a = b
+print(b)
